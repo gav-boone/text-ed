e5662a049c57d554cc82ea330277a545b1285119
gboone11 <gavin.boone@ejgallo.com>
2026-03-27 15:52:21 -0700

    rawMode control and ascii output with text

--- what_if_it_was_c/main.c
+++ what_if_it_was_c/main.c
@@ -1,6 +1,39 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <conio.h>
+#include <windows.h>
+
+DWORD originalMode;
+HANDLE hStdin;
+
+void disableRawMode() {
+    SetConsoleMode(hStdin, originalMode);
+}
+
+void enableRawMode() {
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hStdin, &originalMode);
+
+    DWORD raw = originalMode;
+    raw &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT);
+    SetConsoleMode(hStdin, raw);
+
+    atexit(disableRawMode);
+}
+
+
 
 int main() {
-    printf("Hello, World!\n");
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    };
     return 0;
 }
