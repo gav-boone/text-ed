85554d3c6611ea3755b23cbdf37073e3f085d575
gboone11 <gavin.boone@ejgallo.com>
2026-03-30 09:56:49 -0700

    fixed die bc windows aint linux and tut in linux

--- what_if_it_was_c/main.c
+++ what_if_it_was_c/main.c
@@ -8,29 +8,30 @@
 DWORD originalMode;
 HANDLE hStdin;
 
-// TODO: implement die in RawMode controllers
 void die(const char *s)
 {
-    perror(s);
+    fprintf(stderr, "%s: error %lu\n", s, GetLastError());
     exit(1);
 }
 
 void disableRawMode()
 {
-    if (SetConsoleMode(hStdin, originalMode) == -1)
+    if (!SetConsoleMode(hStdin, originalMode))
         die("SetConsoleMode");
 }
 
 void enableRawMode()
 {
     hStdin = GetStdHandle(STD_INPUT_HANDLE);
-    if (GetConsoleMode(hStdin, &originalMode) == -1)
+    DWORD fileType = GetFileType(hStdin);
+    printf("File type: %lu\n", fileType);
+    if (!GetConsoleMode(hStdin, &originalMode))
         die("SetConsoleMode");
 
     DWORD raw = originalMode;
     raw &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT | ENABLE_VIRTUAL_TERMINAL_INPUT);
 
-    if (SetConsoleMode(hStdin, raw) == -1)
+    if (!SetConsoleMode(hStdin, raw))
         die("SetConsoleMode");
 
     atexit(disableRawMode);
@@ -40,12 +41,11 @@ int main()
 {
     enableRawMode();
 
-    char c;
     DWORD bytesRead;
     while (1)
     {
         char c = '\0';
-        if (ReadConsole(hStdin, &c, 1, &bytesRead, NULL) == -1 && errno != EAGAIN)
+        if (!ReadConsole(hStdin, &c, 1, &bytesRead, NULL) && errno != EAGAIN)
             die("ReadConsole");
 
         if (iscntrl(c))
