7084c2d655a29a4b6b86b746b7734de75c4393e4
gboone11 <gavin.boone@ejgallo.com>
2026-04-01 14:35:47 -0700

    cursor handle tabs

--- what_if_it_was_c/main.c
+++ what_if_it_was_c/main.c
@@ -40,6 +40,7 @@ struct editorConfig {
     int screenRows;
     int screenCols;
     int cx, cy;
+    int rx;
     int numRows;
     int rowoff;
     int coloff;
@@ -227,6 +228,17 @@ void editorAppendRow(char* s, size_t len) {
     E.numRows++;
 }
 
+int editorCxtoRx(erow* row, int cx) {
+    int rx = 0;
+    int j;
+    for (j = 0; j < cx; j++) {
+        if (row->chars[j] == '\t')
+            rx += (TEXT_ED_TAB_STOP - 1) - (rx % TEXT_ED_TAB_STOP);
+        rx++;
+    }
+    return rx;
+}
+
 /* file io */
 ssize_t getline(char** lineptr, size_t* n, FILE* stream) {
     if (!lineptr || !n || !stream) return -1;
@@ -385,17 +397,22 @@ void editorDrawRows(struct abuf* ab) {
 }
 
 void editorScroll() {
+    E.rx = 0;
+    if (E.cy < E.numRows) {
+        E.rx = editorCxtoRx(&E.row[E.cy], E.cx);
+    }
+
     if (E.cy < E.rowoff) {
         E.rowoff = E.cy;
     }
     if (E.cy >= E.rowoff + E.screenRows) {
         E.rowoff = E.cy - E.screenRows + 1;
     }
-    if (E.cx < E.coloff) {
-        E.coloff = E.cx;
+    if (E.rx < E.coloff) {
+        E.coloff = E.rx;
     }
-    if (E.cx >= E.coloff + E.screenCols) {
-        E.coloff = E.cx = E.screenCols + 1;
+    if (E.rx >= E.coloff + E.screenCols) {
+        E.coloff = E.rx = E.screenCols + 1;
     }
 }
 
@@ -412,7 +429,7 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -425,6 +442,7 @@ void editorRefreshScreen() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.numRows = 0;
     E.row = NULL;
     E.rowoff = 0;
